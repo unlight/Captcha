6ad055fba063c0c4b3d4b5e2ec4c3a88fe223692
S <unlights@gmail.com>
2011-05-22 16:19:50 +0400

    New fonts, my code standarts

--- imagettfbox.php
+++ imagettfbox.php
@@ -10,13 +10,13 @@
 $Configuration = array();
 $Configuration['NumChars'] = 3;
 $Configuration['CharRange'] = '/[0-9]/'; // preg_match expression
-$Configuration['CharWidth'] = 35;
-$Configuration['CharHeight'] = 40;
+$Configuration['CharWidth'] = 25;
+$Configuration['CharHeight'] = 25;
 $Configuration['CharSpace'] = $Configuration['CharWidth'] * 0.1;
 $Configuration['Padding'] = 5;
 $Configuration['Noise'] = 5;
 //$Configuration['BorderColor'] = '0,0,0'; // rgb color
-$Configuration['glitf.ttf'] = '/[014]/'; // not this chars (looks ugly)
+$Configuration['glitf.ttf'] = '/[0134]/'; // not this chars (looks ugly)
 $Configuration['Bodiemf.ttf'] = '/1/';
 $Configuration['StarlightSansJL.ttf'] = '/1/';
 $Configuration['AvanteDrp.ttf'] = '/7/';
@@ -33,7 +33,7 @@ ini_set('html_errors', 0);
 session_start();
 
 $Fonts = glob(dirname(__FILE__).'/fonts/*.ttf');
-$CountFonts = Count($Fonts);
+$CountFonts = count($Fonts);
 $PrivateKey = '';
 $CharImages = array();
 
@@ -42,23 +42,23 @@ $Color = RandomColor(210, 255);
 $BackgroundColor = ImageColorAllocate($Dummy, $Color->R, $Color->G, $Color->B);
 ImageDestroy($Dummy);
 
-for($i = 1; $i <= $Configuration['NumChars']; $i++){
+for ($i = 1; $i <= $Configuration['NumChars']; $i++) {
 	
-	$Index = Array_Rand($Fonts);
+	$Index = array_rand($Fonts);
 	$Font = $Fonts[$Index];
-	if($i < Count($Fonts)) unset($Fonts[$Index]);
+	if ($i < count($Fonts)) unset($Fonts[$Index]);
 	
 	$Custom = '';
 	$FontTTF = basename($Font);
-	if(array_key_exists($FontTTF, $Configuration)) $Custom = $Configuration[$FontTTF];
+	if (array_key_exists($FontTTF, $Configuration)) $Custom = $Configuration[$FontTTF];
 	$Char = RandomChar($Configuration['CharRange'], $Custom);
 	
 	$PrivateKey .= $Char;
 
 	$Box = ImageTTFBbox(100, 0, $Font, $Char);
-	$CharWidth = Abs($Box[4] - $Box[0]) * 1.1;
-	$CharHeight = Abs($Box[5] - $Box[1]) * 1.01;
-	if($CharWidth == 0 || $CharHeight == 0) trigger_error('Failed calcualte bounding box. Font: '.basename($Font));
+	$CharWidth = abs($Box[4] - $Box[0]) * 1.1;
+	$CharHeight = abs($Box[5] - $Box[1]) * 1.01;
+	if ($CharWidth == 0 || $CharHeight == 0) trigger_error('Failed calcualte bounding box. Font: '.basename($Font));
 	
 	$CharImage = ImageCreateTrueColor($CharWidth, $CharHeight);
 	ImageFilledRectangle($CharImage, 0, 0, $CharWidth, $CharHeight, $BackgroundColor);
@@ -66,7 +66,7 @@ for($i = 1; $i <= $Configuration['NumChars']; $i++){
 	// shadow 1
 	$C = RandomColor(127, 255);
 	$Shadow = ImageColorAllocate($CharImage, $C->R, $C->G, $C->B);
-	ImageTTFText($CharImage, 99, 0, 20, Abs($Box[5]), $Shadow, $Font, $Char);
+	ImageTTFText($CharImage, 99, 0, 20, abs($Box[5]), $Shadow, $Font, $Char);
 	
 	// shadow 2
 	// not yet
@@ -74,7 +74,7 @@ for($i = 1; $i <= $Configuration['NumChars']; $i++){
 	// text
 	$C = RandomColor(0, 120);
 	$Color = ImageColorAllocate($CharImage, $C->R, $C->G, $C->B);
-	ImageTTFText($CharImage, 100, 0, 2, Abs($Box[5]), $Color, $Font, $Char);
+	ImageTTFText($CharImage, 100, 0, 2, abs($Box[5]), $Color, $Font, $Char);
 	
 	$CharImages[] = array(
 		'Resource' => $CharImage,
@@ -99,7 +99,7 @@ ImageFill($Image, 0, 0, $BackgroundColor); # debug
 $CurX = $Configuration['Padding'];
 
 // todo: transparent text
-foreach($CharImages as $CharImage){
+foreach ($CharImages as $CharImage) {
 	// $Configuration['Padding'] = padding-top
 	ImageCopyResampled($Image, $CharImage['Resource'], $CurX, $Configuration['Padding'], 0, 0, 
 		$Configuration['CharWidth'], $Configuration['CharHeight'], $CharImage['Width'], $CharImage['Height']);
@@ -107,64 +107,66 @@ foreach($CharImages as $CharImage){
 }
 
 // border
-if(array_key_exists('BorderColor', $Configuration)){
-	List($R, $G, $B) = explode(',', $Configuration['BorderColor']);
+if (array_key_exists('BorderColor', $Configuration)) {
+	list($R, $G, $B) = explode(',', $Configuration['BorderColor']);
 	$BorderColor = ImageColorAllocate($Image, $R, $G, $B);
 	ImageRectangle($Image, 0, 0, $W-1, $H-1, $BorderColor);
 }
 
-Header('Content-type: image/jpeg');
+header('Content-type: image/jpeg');
 ImageJpeg($Image);
 
 # ==================================================
 ####################################################
 
-function ErrorHandler($N, $S, $File, $Line){
+function ErrorHandler($N, $S, $File, $Line) {
 	error_log("$S (Line: $Line)\n", 3, date('d-M-Y').'.log');
 	$ErrorImage = ImageCreateTrueColor(150, 50);
 	$ErrorColor = ImageColorAllocate($ErrorImage, 250, 10, 10);
 	$N = 0;
 	foreach(explode(': ', $S) as $Error) ImageString($ErrorImage, 2, 5, $N++ * 12,  $Error, $ErrorColor);
-	@Header('Content-type: image/jpeg');
+	@header('Content-type: image/jpeg');
 	ImageJPEG($ErrorImage);
 	ImageDestroy($ErrorImage);
 	die();
 }
 
-function Clamp($V, $A, $B){
-	if($V > $B) return $B;
-	else if($V < $A) return $A;
+function Clamp($V, $A, $B) {
+	if ($V > $B) return $B;
+	else if ($V < $A) return $A;
 	else return $V;
 }
 
-function RandomChar($Match, $Custom = ''){
+function RandomChar($Match, $Custom = '') {
 	$RandomString = '';
-	do{
-		for($i = 0; $i < 18; $i++) $RandomString .= Chr(Mt_Rand(48, 122));
-		if($Custom != '') $RandomString = preg_replace($Custom, '', $RandomString);
-		$Length = StrLen($RandomString);
+	do {
+		for ($i = 0; $i < 18; $i++) $RandomString .= chr(mt_rand(48, 122));
+		if ($Custom != '') $RandomString = preg_replace($Custom, '', $RandomString);
+		$Length = strlen($RandomString);
 		$C = '';
-		if($Length > 0){
-			$Index = Mt_Rand(0, $Length - 1);
+		if ($Length > 0) {
+			$Index = mt_rand(0, $Length - 1);
 			$C = $RandomString{$Index};
 		}
-	}while(!($C != '' && preg_match($Match, $C)));
+	} while(!($C != '' && preg_match($Match, $C)));
 	return $C;
 }
 
-function RandomColor($Min, $Max){
+function RandomColor($Min, $Max) {
 	$C = new StdClass();
-	$C->R = Mt_Rand($Min, $Max);
-	$C->G = Mt_Rand($Min, $Max);
-	$C->B = Mt_Rand($Min, $Max);
+	$C->R = mt_rand($Min, $Max);
+	$C->G = mt_rand($Min, $Max);
+	$C->B = mt_rand($Min, $Max);
 	return $C;
 }
 
 
-function d(){
+function d() {
 	$a = func_get_args();
 	echo "<pre>";
 	foreach($a as $k => $v) printf("%s\n", print_r($v, 1));
 	die;
 }
+
+
 
